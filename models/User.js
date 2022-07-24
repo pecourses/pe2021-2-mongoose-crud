c06e5d4f9c21b72ae60f087bebbb36a63da5e9f2
Viktoriia <pecourses>
2022-07-24 17:31:24 +0300

    Add user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { EMAIL_VALIDATION_SCHEMA } = require('../utils/validationSchemas');
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: 'string',
+      required: true,
+    },
+    lastName: {
+      type: 'string',
+      required: true,
+    },
+    email: {
+      type: 'string',
+      required: true,
+      validate: {
+        validator: v => EMAIL_VALIDATION_SCHEMA.isValidSync(v),
+      },
+    },
+    gender: {
+      type: 'string',
+      enum: ['male', 'female', 'other'],
+    },
+    birthday: {
+      type: 'date',
+      max: new Date(),
+    },
+    isMarried: {
+      type: 'boolean',
+      default: false,
+    },
+    workExp: {
+      type: 'number',
+      min: 0,
+    },
+  },
+  { versionKey: false }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
