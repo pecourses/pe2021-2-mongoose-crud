d85716d68252d82d65c11cef50e1501ce1a11ad3
Viktoriia <pecourses>
2022-07-24 20:21:24 +0300

    Add controllers with post

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,7 @@
 const createHttpError = require('http-errors');
-const { User } = require('./../models');
+const { default: mongoose } = require('mongoose');
+const _ = require('lodash');
+const { User, Post } = require('./../models');
 
 module.exports.createUser = async (req, res, next) => {
   const { body } = req;
@@ -83,6 +85,77 @@ module.exports.deleteUserById = async (req, res, next) => {
   }
 };
 
-module.exports.createUserPost = async (req, res, next) => {};
+module.exports.createUserPost = async (req, res, next) => {
+  const {
+    params: { userId },
+    body,
+  } = req;
 
-module.exports.getUserPosts = async (req, res, next) => {};
+  try {
+    const foundUser = await User.findById(userId);
+    if (!foundUser) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+    const newPostInstance = new Post({
+      ...body,
+      userId: mongoose.Types.ObjectId(userId),
+    });
+    const createdPost = await newPostInstance.save();
+    if (!createdPost) {
+      return next(createHttpError(400, 'Bad Request'));
+    }
+
+    // Метод инстанса модели toObject() возвращает JS-объект с полезной нагрузкой
+    const preparedPost = _.omit(createdPost.toObject(), ['updatedAt']);
+    res.status(201).send({ data: preparedPost });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getUserPosts = async (req, res, next) => {
+  const {
+    params: { userId },
+  } = req;
+
+  try {
+    // 1
+    // const foundUser = await User.findById(userId);
+    // if (!foundUser) {
+    //   return next(createHttpError(404, 'User Not Found'));
+    // }
+
+    // const foundPosts = await Post.find({
+    //   userId: mongoose.Types.ObjectId(userId),
+    // });
+
+    // if (!foundPosts) {
+    //   next(createHttpError(400, 'Bad Request'));
+    // }
+
+    // 2
+    // aggregate (+ match, lookup, project)
+    // работает аналогично aggregate в MongoDB:
+    // - match - фильтр,
+    // - lookup - соединение с документами другой коллекции по указанным критериям
+    // - project - проекция (какие поля полученных документов включать в результат)
+
+    const foundPosts = await User.aggregate()
+      .match({ _id: mongoose.Types.ObjectId(userId) })
+      .lookup({
+        from: 'posts',
+        localField: '_id',
+        foreignField: 'userId',
+        as: 'usersToPosts',
+      })
+      .project({ usersToPosts: 1, _id: 0 });
+
+    if (!foundPosts.length) {
+      return next(createHttpError(404, 'User Not Found'));
+    }
+
+    res.status(200).send({ data: foundPosts });
+  } catch (err) {
+    next(err);
+  }
+};
